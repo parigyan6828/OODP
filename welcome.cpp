99445a8b584cca7a458271099a943176112c3c96
sivakumarcse83 <78533956+sivakumarcse83@users.noreply.github.com>
2026-03-12 10:20:04 +0530

    commit

---
+++ welcome.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char name[25];
+    int age;
+    cout<<"Enter your name:";
+    cin>>name;
+    cout<<endl;
+    cout<<"Enter your age:";
+    cin>>age;
+    cout<<"Your Name is:"<<name<<endl;
+    cout<<"Your Age is:"<<age;
+    return 0;
+}
